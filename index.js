24712bf7ceeb2d1e26be620343c3b15b13e1caef
RobertasAnkudovicius <r.ankudovicius@gmail.com>
2023-04-24 20:07:44 +0300

    Added cors library

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
 
 const array = [324, 634, 234, 6544, 23, 76, 234, 654];
 
@@ -16,12 +12,9 @@ app.get("/getRandomNumbers", (req, res) => {
 
 app.get("/getRandomNumber", (req, res) => {
   const randomNum = Math.random();
-  return res.status(200).json({ randomNumbers: randomNum });
+  return res.status(404).json({ randomNumbers: randomNum });
 });
 
 app.listen(8081, () => {
   console.log("Your app is alive!!!!!");
 });
-
-// var cors = require("cors");
-// app.use(cors());

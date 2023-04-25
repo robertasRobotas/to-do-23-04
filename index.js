26a753e88babfecc7bbb68a441692cdc961fb69e
RobertasAnkudovicius <r.ankudovicius@gmail.com>
2023-04-25 18:31:52 +0300

    Changing application to cars...

--- index.js
+++ index.js
@@ -4,15 +4,10 @@ var cors = require("cors");
 
 app.use(cors());
 
-const array = [324, 634, 234, 6544, 23, 76, 234, 654];
+const cars = ["Audi, BMW, Subaru"];
 
-app.get("/getRandomNumbers", (req, res) => {
-  return res.status(200).json({ randomNumbers: array });
-});
-
-app.get("/getRandomNumber", (req, res) => {
-  const randomNum = Math.random();
-  return res.status(404).json({ randomNumbers: randomNum });
+app.get("/getAllCars", (req, res) => {
+  return res.status(200).json({ cars: cars });
 });
 
 app.listen(8081, () => {
